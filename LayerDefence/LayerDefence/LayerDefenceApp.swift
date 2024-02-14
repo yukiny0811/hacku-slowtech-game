30eea5edb2f239b3909496a950f48be8bfbbb521
yukiny0811 <yukiny0811@gmail.com>
2024-02-15 03:34:37 +0900

    initial commit

---
+++ LayerDefence/LayerDefence/LayerDefenceApp.swift
@@ -0,0 +1,18 @@
+//
+//  LayerDefenceApp.swift
+//  LayerDefence
+//
+//  Created by Yuki Kuwashima on 2024/02/15.
+//
+
+import SwiftUI
+import LayerDefenceKit
+
+@main
+struct LayerDefenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
